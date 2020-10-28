1e558a0fa5842717eb38d9ee6cb4e488118bfe2a
itzelrincon <72486961+itzelrincon@users.noreply.github.com>
2020-10-28 12:55:03 -0600

    Add files via upload

---
+++ generico/MethodUsage.java
@@ -0,0 +1,8 @@
+package generico;
+public class MethodUsage {
+    
+    Pair<Integer, String> p1 = new OrderedPair<>(1, "apple");
+    Pair<Integer, String> p2 = new OrderedPair<>(2, "pear");
+    boolean same = Method.compare(p1, p2);
+   
+}
